b75c4019d4957c525c880a5341a079aabc25399e
Citlalli DMG <citlallidmg@gmail.com>
2018-08-22 10:50:10 -0500

    agrega funcion printMovies que recibe los datos de las peliculas a imprimir, falta que se pinten en pantalla

--- src/js/data.js
+++ src/js/data.js
@@ -1,12 +1,11 @@
 window.getMovies = (api, search) => {
   const url = `http://www.omdbapi.com/?apikey=${api}&s=${search}&type=movie`;
-  console.log(url);
 
   fetch(url)
     .then(response => response.json())
     .then((res) => {
-      console.log(res);
-      getData(res);
+      let moviesData = getData(res);
+      printMovies(moviesData);
     })
     .catch((error) => {
       console.log(error);
@@ -15,6 +14,5 @@ window.getMovies = (api, search) => {
 
 window.getData = (data) => {
   let movies = data.Search;
-  console.log(movies);
   return movies;
 };
--- src/js/index.js
+++ src/js/index.js
@@ -4,23 +4,38 @@ let search = '';
 
 // Get buttons
 const buttons = document.getElementsByClassName('entry');
-console.log(buttons);
 
-const bntSuperman = document.getElementById('option-1');
-const bntBatman = document.getElementById('option-2');
-const bntWonderWoman = document.getElementById('option-3');
-
-const getEntry = () => {
+const getId = () => {
   search = event.target.id;
+  return search;
+};
+
+const getEntry = (id) => {
+  switch (id) {
+  case 'option-1':
+    search = 'Superman';
+    break;
+  case 'option-2':
+    search = 'Batman';
+    break;
+  case 'option-3':
+    search = 'Wonder+Woman';
+    break;
+  }
   return search;
 };
 
 // Listen to buttons
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', () => {
-    let id = getEntry();
-    console.log(id);
+    let id = getId();
+    let search = getEntry(id);
+    getMovies(apiKey, search);
   });
+};
+
+const printMovies = (moviesToPrint) =>{
+  console.log(moviesToPrint);
 };
 
 // Function for the button in the first reach
