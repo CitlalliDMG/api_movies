ad514e2b11a1b60a67eb3f527d77406ba0256eb6
Citlalli DMG <citlallidmg@gmail.com>
2018-08-22 10:26:15 -0500

    agrega botones solicitados en el segundo alcance y funcion que los escucha

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,33 @@
+// Declare API Key and Search String
 const apiKey = 'a057ebc5';
-const search = 'Wonder+Woman';
+let search = '';
 
-const btn = document.getElementById('get-data');
+// Get buttons
+const buttons = document.getElementsByClassName('entry');
+console.log(buttons);
 
-btn.addEventListener('click', (event) => {
-  getMovies(apiKey, search);
-});
+const bntSuperman = document.getElementById('option-1');
+const bntBatman = document.getElementById('option-2');
+const bntWonderWoman = document.getElementById('option-3');
+
+const getEntry = () => {
+  search = event.target.id;
+  return search;
+};
+
+// Listen to buttons
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', () => {
+    let id = getEntry();
+    console.log(id);
+  });
+};
+
+// Function for the button in the first reach
+// const search = 'Wonder+Woman';
+// const btn = document.getElementById('get-data');
+
+// btn.addEventListener('click', (event) => {
+//   getMovies(apiKey, search);
+// });
 
